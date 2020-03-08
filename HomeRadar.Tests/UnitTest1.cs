97dec9a2b0be740dedf981c9d00972eefeb9332b
Simon Crowther <simon.crowther95@hotmail.co.uk>
2020-03-08 17:20:04 +0000

    Added test except there was a test failure

--- HomeRadar.Tests/UnitTest1.cs
+++ HomeRadar.Tests/UnitTest1.cs
@@ -5,10 +5,21 @@ namespace HomeRadar.Tests
 {
     public class UnitTest1
     {
+        public double SquareRoot(double input)
+        {
+            return input / 2;
+        }
+
         [Fact]
         public void Test1()
         {
-
+            // Define a test input and output value:
+            double expectedResult = 2.0;
+            double input = expectedResult * expectedResult;
+            // Run the method under test:
+            double actualResult = SquareRoot(input);
+            // Verify the result:
+            Assert.AreEqual(expectedResult, actualResult);
         }
     }
 }
