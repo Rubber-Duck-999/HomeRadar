fd066e98880b8874f018048fcd5fd7e44a5ea36f
Aaron Waddingham <awaddingham@hotmail.com>
2020-03-30 16:17:55 +0100

    Added missing template and converter to display menu items in hamburger menu. Updated MenuView to include the collection of menu items and display using the template.
    Still need to include the tapped behaviour.
    
    Related work items: #3

---
+++ HomeRadar/Converters/MenuIconConverter.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="MenuIconConverter.cs" company="FutureInnovationTech">
+// Copyright (c) FutureInnovationTech. All rights reserved.
+// </copyright>
+
+namespace HomeRadar.Converters
+{
+  using System;
+  using System.Globalization;
+  using HomeRadar.Models;
+  using Xamarin.Forms;
+
+  public class MenuIconConverter : IValueConverter
+  {
+    /// <inheritdoc/>
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      var type = (MenuItemType)value;
+
+      switch (type)
+      {
+        case MenuItemType.HomeRadar:
+          return "ic_home.png";
+        default:
+          return string.Empty;
+      }
+    }
+
+    /// <inheritdoc />
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
