42e6ccabd45c45333b912e658bd72d319035cf00
Aaron Waddingham <awaddingham@hotmail.com>
2020-03-31 16:29:13 +0100

    Added ConnectivityWrapper for Xamarin.Essentials and as an abstraction layer for testing.
    Related work item: #2

---
+++ HomeRadar/Wrappers/Contracts/IConnectivityWrapper.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="IConnectivityWrapper.cs" company="FutureInnovationTech">
+// Copyright (c) FutureInnovationTech. All rights reserved.
+// </copyright>
+
+namespace HomeRadar.Wrappers
+{
+  using Xamarin.Essentials;
+
+  /// <summary>
+  /// Defines the interface for wrapper 3rd party connectivity providers.
+  /// </summary>
+  public interface IConnectivityWrapper
+  {
+    /// <summary>
+    /// Provides the type of access currently available.
+    /// </summary>
+    /// <returns>A NetworkAccess value.</returns>
+    NetworkAccess TypeOfAccess();
+  }
+}
