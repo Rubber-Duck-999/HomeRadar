1316fcfba87268e1c1107f28ce786e0a4adc09a0
Aaron Waddingham <awaddingham@hotmail.com>
2020-04-07 13:10:56 +0100

    Created a mock IConnectivityWrapper for mocking 3rd party functionality.

---
+++ HomeRadar.Tests/MockImplementations/MockConnectivityWrapper.cs
@@ -0,0 +1,56 @@
+﻿namespace HomeRadar.Tests.MockImplementations
+{
+  using System;
+  using System.Collections.Generic;
+  using Wrappers.Contracts;
+  using Xamarin.Essentials;
+
+  /// <summary>
+  /// Mocks an IConnectivityWrapper implementation.
+  /// </summary>
+  public class MockConnectivityWrapper : IConnectivityWrapper
+  {
+    public MockConnectivityWrapper(NetworkAccess testAccess)
+    {
+      currentAccess = testAccess;
+    }
+
+    static event EventHandler<ConnectivityChangedEventArgs> ConnectivityChangedInternal;
+
+    static NetworkAccess currentAccess;
+
+    public event EventHandler<ConnectivityChangedEventArgs> ConnectivityChanged
+    {
+      add
+      {
+        var wasRunning = ConnectivityChangedInternal != null;
+
+        ConnectivityChangedInternal += value;
+      }
+
+      remove
+      {
+        var wasRunning = ConnectivityChangedInternal != null;
+
+        ConnectivityChangedInternal -= value;
+      }
+    }
+
+    public static void OnConnectivityChanged(NetworkAccess access, IEnumerable<ConnectionProfile> profiles)
+      => OnConnectivityChanged(new ConnectivityChangedEventArgs(access, profiles));
+
+    public static void OnConnectivityChanged(ConnectivityChangedEventArgs e)
+    {
+      if (currentAccess != e.NetworkAccess)
+      {
+        currentAccess = e.NetworkAccess;
+        ConnectivityChangedInternal?.Invoke(null, e);
+      }
+    }
+
+    public NetworkAccess TypeOfAccess()
+    {
+      return currentAccess;
+    }
+  }
+}
