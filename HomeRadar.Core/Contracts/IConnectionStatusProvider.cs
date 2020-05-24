686a33b7fcaef124c8535888fa8652cfffe11f64
Aaron Waddingham <awaddingham@hotmail.com>
2020-05-24 12:26:18 +0100

    Added Contracts and NetworkAccess enum to library.
    Related work item: #25

---
+++ HomeRadar.Core/Contracts/IConnectionStatusProvider.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="IConnectionStatusProvider.cs" company="FutureInnovationTech">
+// Copyright (c) FutureInnovationTech. All rights reserved.
+// </copyright>
+
+namespace HomeRadar.Core.Contracts
+{
+  /// <summary>
+  /// Defines the interface for confirming the device is connected to a network.
+  /// </summary>
+  public interface IConnectionStatusProvider
+  {
+    /// <summary>
+    /// Gets a value indicating whether or not the device is connected to a network.
+    /// </summary>
+    bool IsConnected { get; }
+
+    /// <summary>
+    /// Checks if the device is currently connected to a network.
+    /// </summary>
+    void CheckConnection();
+  }
+}
