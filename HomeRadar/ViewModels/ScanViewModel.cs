c6629ab580d0a2c0b3a095a8daa11541f155f452
Aaron Waddingham <awaddingham@hotmail.com>
2020-06-10 10:39:27 +0100

    Initial design for Scan. Includes new ScanView and ScanViewModel.
    Related work item: #38

---
+++ HomeRadar/ViewModels/ScanViewModel.cs
@@ -0,0 +1,30 @@
+﻿// <copyright file="ScanViewModel.cs" company="FutureInnovationTech">
+// Copyright (c) FutureInnovationTech. All rights reserved.
+// </copyright>
+
+namespace HomeRadar.ViewModels
+{
+  using HomeRadar.Contracts;
+  using HomeRadar.ViewModels.Base;
+
+  /// <summary>
+  /// The following class defines the ViewModel for the Scan view.
+  /// </summary>
+  public class ScanViewModel : ViewModelBase
+  {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ScanViewModel"/> class.
+    /// </summary>
+    /// <param name="navigationService">App navigation service.</param>
+    public ScanViewModel(INavigationService navigationService)
+      : base(navigationService)
+    {
+      this.ShortTitle = "Scan";
+    }
+
+    /// <summary>
+    /// Gets the scan button text.
+    /// </summary>
+    public string ScanButtonText => "Scan";
+  }
+}
---
+++ HomeRadar/Views/ScanView.xaml.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="ScanView.xaml.cs" company="FutureInnovationTech">
+// Copyright (c) FutureInnovationTech. All rights reserved.
+// </copyright>
+
+namespace HomeRadar.Views
+{
+  using Xamarin.Forms;
+  using Xamarin.Forms.Xaml;
+
+  /// <summary>
+  /// The following class defines the code behind for the Scan View page.
+  /// </summary>
+  [XamlCompilation(XamlCompilationOptions.Compile)]
+  public partial class ScanView : ContentPage
+  {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ScanView"/> class.
+    /// </summary>
+    public ScanView()
+    {
+      this.InitializeComponent();
+    }
+  }
+}
