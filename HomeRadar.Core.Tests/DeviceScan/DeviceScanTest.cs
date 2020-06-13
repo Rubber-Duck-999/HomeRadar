ee8daf8d91dc506b43d15f9a8d8900df227be716
Aaron Waddingham <awaddingham@hotmail.com>
2020-06-13 11:28:50 +0100

    Updated test with suggested changes to decouple the tests from the underlying platform.
    Related work item: #32

--- HomeRadar.Core.Tests/DeviceScan/DeviceScanTest.cs
+++ HomeRadar.Core.Tests/DeviceScan/DeviceScanTest.cs
@@ -4,7 +4,7 @@
 
 namespace HomeRadar.Core.Tests.DeviceScan
 {
-  using System.Net.NetworkInformation;
+  using System;
   using FluentAssertions;
   using Xunit;
   using DeviceScan = Model.DeviceScan;
@@ -21,7 +21,7 @@ namespace HomeRadar.Core.Tests.DeviceScan
     public void DeviceScan_RunArpSuccessfully()
     {
       // Arrange
-      DeviceScan scan = new DeviceScan();
+      var scan = new DeviceScan();
       Assert.True(string.IsNullOrEmpty(scan.NetworkAdapter.HostName));
       Assert.Equal("WINDOWS", scan.NetworkAdapter.OperatingSystem.ToString());
       scan.NetworkAdapter.OperatingSystem.ToString().Should().Be("WINDOWS");
@@ -36,25 +36,34 @@ namespace HomeRadar.Core.Tests.DeviceScan
       // ToDo: Note - Assertions should be
       // Assertion(expected, actual);
 
+      // ToDo: Tests should not depend on the platform they run.
+      // ToDo: Abstract the platform dependency to an interface so the platform can be plugged in.
+      // This will allow us to plug in a test platform at the test stage and use the actual platform at runtime.
+      // This also allows us to abstract away code that isn't ours e.g.  NetworkInterface.GetAllNetworkInterfaces() used in 
+      // DeviceScan.GetDeviceDetails().
+
       // Strings should be set
-      Assert.False(string.IsNullOrEmpty(scan.NetworkAdapter.HostName));
-      Assert.False(string.IsNullOrEmpty(scan.NetworkAdapter.NetworkName));
-      Assert.False(string.IsNullOrEmpty(scan.NetworkAdapter.Ipv4));
-      Assert.False(string.IsNullOrEmpty(scan.NetworkAdapter.MacAddress));
+      //Assert.False(string.IsNullOrEmpty(scan.NetworkAdapter.HostName));
+      //Assert.False(string.IsNullOrEmpty(scan.NetworkAdapter.NetworkName));
+      //Assert.False(string.IsNullOrEmpty(scan.NetworkAdapter.Ipv4));
+      //Assert.False(string.IsNullOrEmpty(scan.NetworkAdapter.MacAddress));
 
       // These assertions are the same as the above but use the Fluent Assertions framework.
       // I think they are nicer to read.
       // Up to you whether you use fluent assertions or standard xUnit, I prefer fluent.
-      string.IsNullOrEmpty(scan.NetworkAdapter.HostName).Should().BeFalse();
-      string.IsNullOrEmpty(scan.NetworkAdapter.NetworkName).Should().BeFalse();
-      string.IsNullOrEmpty(scan.NetworkAdapter.Ipv4).Should().BeFalse();
-      string.IsNullOrEmpty(scan.NetworkAdapter.MacAddress).Should().BeFalse();
+      //string.IsNullOrEmpty(scan.NetworkAdapter.HostName).Should().BeFalse();
+      //string.IsNullOrEmpty(scan.NetworkAdapter.NetworkName).Should().BeFalse();
+      //string.IsNullOrEmpty(scan.NetworkAdapter.Ipv4).Should().BeFalse();
+      //string.IsNullOrEmpty(scan.NetworkAdapter.MacAddress).Should().BeFalse();
 
       // Status should be up
-      Assert.Equal(OperationalStatus.Up, scan.NetworkAdapter.OperationalStatus);
-      scan.NetworkAdapter.OperationalStatus.Should().Be(OperationalStatus.Up);
+      //Assert.Equal(OperationalStatus.Up, scan.NetworkAdapter.OperationalStatus);
+      //scan.NetworkAdapter.OperationalStatus.Should().Be(OperationalStatus.Up);
 
-      scan.FindDevices();
+      // ToDo: Why are we doing this?
+      //scan.FindDevices();
+
+      throw new NotImplementedException();
     }
   }
 }
