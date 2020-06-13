24773659c811e508e68a0a2587e97a6ad6e98663
Aaron Waddingham <awaddingham@hotmail.com>
2020-06-13 13:24:05 +0100

    Added enum to contain scan types.
    Related work item: #38

---
+++ HomeRadar/Enums/ScanTypes.cs
@@ -0,0 +1,37 @@
+﻿// <copyright file="ScanTypes.cs" company="FutureInnovationTech">
+// Copyright (c) FutureInnovationTech. All rights reserved.
+// </copyright>
+
+namespace HomeRadar.Enums
+{
+  /// <summary>
+  /// Enum defining Scan Types.
+  /// </summary>
+  public enum ScanTypes
+  {
+    /// <summary>
+    /// Represents no scan type.
+    /// </summary>
+    None,
+
+    /// <summary>
+    /// Represents the All scan type.
+    /// </summary>
+    All,
+
+    /// <summary>
+    /// Represents the DevicesOnly scan type.
+    /// </summary>
+    DevicesOnly,
+
+    /// <summary>
+    /// Represents the Intensive scan type.
+    /// </summary>
+    Intensive,
+
+    /// <summary>
+    /// Represents the Intrusive scan type.
+    /// </summary>
+    Intrusive,
+  }
+}
